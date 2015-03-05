716506a0310fd58e9946a296b238f15839256c9e
Krystian Duma <git@kduma.tk>
2015-03-05 16:13:25 +0100

    First Commit

---
+++ Guidable.php
@@ -0,0 +1,64 @@
+<?php
+namespace KDuma\Eloquent;
+
+use Rhumsaa\Uuid\Uuid;
+use Config;
+use Hashids\Hashids;
+
+
+/**
+ * Class Guidable
+ * @package KDuma\Eloquent
+ */
+trait Guidable {
+
+
+    /**
+     * @return string
+     */
+    protected function makeGuid()
+    {
+
+        $guid = Uuid::uuid4()->toString();
+
+        if($guid == $this->guid)
+            return $guid;
+
+        $rowCount = \DB::table($this->getTable())->where('guid', $guid)->count();
+        if ($rowCount > 0) {
+            return $this->makeGuid();
+        } else {
+            return $guid;
+        }
+    }
+
+    /**
+     *
+     */
+    public function newGuid(){
+        $this->guid = $this->makeGuid();
+    }
+
+    /**
+     * @param array $options
+     * @return mixed
+     */
+    public function save(array $options=[]){
+        if($this->guid == '')
+            $this->newGuid();
+        return parent::save($options);
+    }
+
+
+    /**
+     * @param $query
+     * @param $guid
+     * @return mixed
+     */
+    public function scopeWhereGuid($query, $guid)
+    {
+        return $query->where('guid', $guid);
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# L5-eloquent-guidable
+Eases using and generating guid's in Laravel Eloquent models.
+
+# Setup
+Add the package to the require section of your composer.json and run `composer update`
+
+    "kduma/eloquent-guidable": "~1.0"
+
+# Prepare models
+Inside your model (not on top of file) add following lines:
+    
+    use \KDuma\Eloquent\Guidable;
+
+In database create `guid` string field. If you use migrations, you can use following snippet:
+
+    $table->string('guid')->unique();
+
+# Usage
+By default it generates slug on first save.
+
+- `$model->newGuid()` - Generate new guid. (Remember to save it by yourself)
+- `Model::whereGuid($slug)->first()` - Find by guid. (`whereGuid` is query scope)
+   
+
+# euantorano
+
+A special thanks to [euantorano](http://forumsarchive.laravel.io/viewtopic.php?id=6629#6), an original code creator that this package is based on.
+
+# Packagist
+View this package on Packagist.org: [kduma/eloquent-guidable](https://packagist.org/packages/kduma/eloquent-guidable)
