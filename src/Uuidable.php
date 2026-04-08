60b053c675193e7d07283fe6875a9b4f0dd085dc
Krystian Duma <krystian@muzik.pl>
2026-04-08 16:28:21 +0200

    Fix: emit each deprecation notice at most once per request using static cache

--- src/Uuidable.php
+++ src/Uuidable.php
@@ -74,10 +74,15 @@ trait Uuidable
         }
 
         if (property_exists($this, $legacyProperty)) {
-            trigger_error(
-                "Using \${$legacyProperty} on " . static::class . ' is deprecated. Use #[HasUuid] attribute instead.',
-                E_USER_DEPRECATED,
-            );
+            static $fired = [];
+            $key = static::class . '::$' . $legacyProperty;
+            if (!isset($fired[$key])) {
+                $fired[$key] = true;
+                trigger_error(
+                    "Using \${$legacyProperty} on " . static::class . ' is deprecated. Use #[HasUuid] attribute instead.',
+                    E_USER_DEPRECATED,
+                );
+            }
 
             return $this->{$legacyProperty} ?? $default;
         }
